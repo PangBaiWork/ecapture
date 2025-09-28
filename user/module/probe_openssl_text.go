987ca216ab1ceef4f9a5f89a025d24bd631897ca
PangBai <63479821+PangBaiWork@users.noreply.github.com>
2025-09-28 16:18:10 +0800

    fix space

--- user/module/probe_openssl_text.go
+++ user/module/probe_openssl_text.go
@@ -50,7 +50,7 @@ func (m *MOpenSSLProbe) setupManagersText() error {
 				Section:          "uprobe/SSL_write",
 				EbpfFuncName:     "probe_entry_SSL_write",
 				AttachToFuncName: "SSL_write",
-				UprobeOffset:      0x3A8614,
+				UprobeOffset:     0x3A8614,
 				BinaryPath:       binaryPath,
 			},
 			{
